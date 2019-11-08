6683906c923d735f812b5fbd81564e3d51578fc7
yuga <yuga@senthink.com>
2019-11-08 14:30:54 +0800

    新建项目

---
+++ src/code/HelloTools.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace code;
+
+/**
+* 
+*/
+class HelloTools
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public static function greet()
+	{
+		echo "Hello!", PHP_EOL;
+	}
+}
